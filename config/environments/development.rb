9f8469c8af3803816e3ae744c7e33ee23acdf26d
Alex Rozumey <brain-geek@yandex.ua>
2014-02-07 14:56:23 +0200

    Customise generators config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,15 @@ TimeTracker::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Generators config
+  config.generators do |g|
+    g.assets      false
+    g.javascripts false
+    g.stylesheets false
+    g.test_framework = :rspec
+    g.fixture_replacement = :fabrication
+  end
   
   # Mailer
   config.action_mailer.raise_delivery_errors = true
